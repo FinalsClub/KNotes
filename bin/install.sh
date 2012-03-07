8df06c8c8b962b83ba255b3c85d5c8a97bfb808e
Seth Woodworth <seth@sethish.com>
2012-03-07 12:45:08 -0500

    stubbing out install script

---
+++ bin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# -------
+# This script checks node & npm versions, installs those if needed.
+# Then the script checksout the relevant node_modules
+# -------
+
+# TODO: check node version
+# TODO: install node 0.6.11 if node is not present
+# TODO: If node --version < 0.6.0, exit with error message
+
+# TODO: check npm version
+# TODO: install recent/latest npm if not present
+
+# TODO: checkout any git submodules required by the project
+
+# TODO: run `npm install .`
