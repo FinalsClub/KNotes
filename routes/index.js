d0e60e4dccb7597857c2d998e47998a245d2762d
Seth Woodworth <seth@sethish.com>
2012-03-11 15:42:25 -0400

    adding a stock route

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+
+/*
+ * GET home page.
+ * TODO: use this method initially to pass the backbone.js template to the client
+ *      use other routes in this folder to handle other requests to respond via json
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Values to be rendered go here' })
+};
