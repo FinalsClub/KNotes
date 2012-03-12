38a8bb01eef9ca60955fce3f15378453c50abae6
David Brodsky <davidpbrodsky@gmail.com>
2012-03-12 15:12:34 -0700

    html5 dragNddrop file display

--- schema.js
+++ schema.js
@@ -3,11 +3,18 @@
 var mongoose    = require('mongoose')
   , Schema      = mongoose.Schema;
 
+mongoose.connect('mongodb://localhost/data/db/');
+
+var RATING_MAX = 100;
+var RATING_MIN = 0;
+
 var ExampleSchema = new Schema({
   filename : String
   , owner : Schema.ObjectId
   , size : Number
+  , rating: Number({default:0, max:RATING_MAX, min:RATING_MIN})
 });
+
 // filename | placeholder for path to local file or to s3 file
 // owner | foreign key to a user object, TODO: Create user schema
 // size | size of the file in bytes, 
